d37b69fc790e063dc6f692a8428e6e2ab9489b17
shish <shishkin321senya@gmail.com>
2025-03-03 00:54:58 +0300

    initialization

---
+++ backend/app/src/main/java/com/startupgame/StartUpGameApplication.java
@@ -0,0 +1,11 @@
+package com.startupgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartUpGameApplication.class, args);
+    }
+}

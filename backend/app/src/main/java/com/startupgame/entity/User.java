42ccc9a6a855f076bcc57dc700d5fd26fe7a8580
Nick Menshikov <nikmenshikov1@gmail.com>
2025-03-13 17:13:52 +0300

    add database configuration init initial tables

---
+++ backend/app/src/main/java/com/startupgame/entity/User.java
@@ -0,0 +1,15 @@
+package com.startupgame.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
+}

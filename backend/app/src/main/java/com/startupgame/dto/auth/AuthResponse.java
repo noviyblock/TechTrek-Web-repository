5a196beb130ce8d3ccc59f9dc6899fea8c986221
Nick Menshikov <nikmenshikov1@gmail.com>
2025-03-31 18:12:15 +0300

    add implementation of register endpoint

--- backend/app/src/main/java/com/startupgame/dto/auth/AuthResponse.java
+++ backend/app/src/main/java/com/startupgame/dto/auth/AuthResponse.java
@@ -1,12 +1,14 @@
 package com.startupgame.dto.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
 
-    private String accessToken;
-    private String refreshToken;
-    private String tokenType = "Bearer";
-    private RegisterRequest user;
+    private String token;
+    private String username;
 }

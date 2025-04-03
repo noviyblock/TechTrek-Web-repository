77a1c4b06a9b6304d9a7b55399b0ecd83df89544
Nick Menshikov <nikmenshikov1@gmail.com>
2025-04-03 15:19:41 +0300

    add implementation of login endpoint, add table for refresh token

---
+++ backend/app/src/main/java/com/startupgame/exception/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package com.startupgame.exception;
+
+public class BadCredentialsException extends RuntimeException {
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}

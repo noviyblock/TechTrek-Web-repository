01bd9e76b63cd0301d8bebdbb73d8c479c0386eb
Nick Menshikov <nikmenshikov1@gmail.com>
2025-04-06 20:46:39 +0300

    add save token to db, test for suth service, docs for service methods

--- backend/app/src/main/java/com/startupgame/repository/RefreshTokenRepository.java
+++ backend/app/src/main/java/com/startupgame/repository/RefreshTokenRepository.java
@@ -1,10 +1,12 @@
 package com.startupgame.repository;
 
 import com.startupgame.entity.RefreshToken;
+import com.startupgame.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    //TODO
+    void deleteByUser(User user);
+    Optional<RefreshToken> findByTokenValue(String token);
 }

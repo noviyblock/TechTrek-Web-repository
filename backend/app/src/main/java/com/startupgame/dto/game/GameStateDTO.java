f94e2fe4824a381ea87b0e9565901f8a5d242b38
Nick Menshikov <nikmenshikov1@gmail.com>
2025-04-18 00:05:22 +0300

    Add DTO for game state
    Add GameStateDTO for correct response to frontend when starting the game
    Change test for startGame method
    Formatted code

---
+++ backend/app/src/main/java/com/startupgame/dto/game/GameStateDTO.java
@@ -0,0 +1,29 @@
+package com.startupgame.dto.game;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class GameStateDTO {
+    private Long gameId;
+    private String companyName;
+    private int stage;
+    private int turnNumber;
+    private int monthsPassed;
+    private long missionId;
+
+    private long money;
+    private int technicReadiness;
+    private int productReadiness;
+    private int motivation;
+    private int reputation;
+
+    private int juniors;
+    private int middles;
+    private int seniors;
+
+    private String situationText;
+}

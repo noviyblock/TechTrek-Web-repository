da1a6be04695c94fe58b4018b8fbf204daf5cb65
Nick Menshikov <nikmenshikov1@gmail.com>
2025-06-04 23:13:49 +0300

    add .env path into docker-compose.yaml

--- frontend/src/shared/constants.tsx
+++ frontend/src/shared/constants.tsx
@@ -87,7 +87,7 @@ export const categories: CardButtonProps[] = [
   },
 ];
 
-export const backendURL = "http://95.174.91.208:8080";
+export const backendURL = "";
 
 export const choosableList: ChoosableTextProps[] = [
   {
